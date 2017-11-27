a017d5771dbba260d852216f293bc51746b75e31
GaetanGalati <32631949+GaetanGalati@users.noreply.github.com>
2017-11-27 10:05:38 +0100

    Update Jeu_De_Loie.pas

--- Jeu_De_Loie.pas
+++ Jeu_De_Loie.pas
@@ -1,85 +1,85 @@
 
--{ALGORITHME Jeu_De_Loie
--//BUT: Réaliser un jeu de l'oie
--//ENTREES: La validation, et la valeure du dé
--//SORTIES: Le jeu nous dit se qu'il se passe
--
--CONST // Les constantes désignent les "cases spéciale" du jeu
--	
--	debut <- 0
--	fin <- 66
--	double <- 9
--	sixtroi <- 63
--	Gameover <- 58
--	minD <- 2
--	maxD <- 12
--
--VAR
--	de: ENTIER
--	joueur: ENTIER
--// "de" = le jet du dé, "joueur" = le placement du joueur 
--
--DEBUT
--
--	de <- 0
--	joueur <- 0
--
--	ECRIRE '           LE JEU DE LOIE               '
--	ECRIRE '         Appuyer sur entrer !           '
--	LIRE
--
--	ECRIRE 'Votre Oie est a la case départ: ',joueur
--
--	REPETER //Grande boucle  elle se répete jusqu'a se que joueur = 66(fin).
--
--		ECRIRE 'Appuyer sur entrer pour continuer'
--		LIRE
--		ECRIRE 'lancez les deux dés'
--		LIRE de
--
--		REPETER //Petite boucle elle se répete si le joueurs rentre un nombre plus petit que 2 ou plus grand que 12
--
--
--			ECRIRE ('Vous etes sur la case ','[',joueur,']', 'Appuyer sur entrer pour continuer');
--			ECRIRE ('lancez les deux des (Choisiez un nombre entre 2 et 12)');
--			LIRE (de);
--			SI (de > maxD) ET (de < minD) AlORS
--				ECRIRE ' Entrez un nombre valide !';
--						
--		JUSQU'A ((de <= maxD) ET (de >= minD));
--
--
--		joueur <-(de + joueur)
--
--		ECRIRE 'Bravo ! vous allez jusqu'a la case ','[',joueur,']',' Voyons se qui se passe ici...'
--
--		SI( joueur mod double = 0 ) ET ( joueur <> sixtroi ) ALORS
--			joueur <- de + joueur
--			ECRIRE '[*2]'
--			ECRIRE 'Incoyable ! votre jet est doublé '
--		FINSI
--
--		SI (joueur = Gameover) ALORS
--			joueur <- 0
--			ECRIRE '[X]'
--			ECRIRE 'Oh non ! vous étes sur la case téte de mort ! '
--			ECRIRE 'Vous retournez a la case depart... '
--		FINSI
--
--		SI (joueur > fin) ALORS
--			joueur <- (joueur - (joueur - fin))
--			ECRIRE 'Oula vous allez trop loin !'
--			ECRIRE 'Retournez donc a la case: ',joueur
--		FINSI
--
--	JUSQU'A (joueur = fin)
--	FINREPETER
--
--	ECRIRE 'Félicitation ! Vous étes a la case [66] vous avez donc gagné ! Bravo !'
--
--FIN
--}
--
+{ALGORITHME Jeu_De_Loie
+//BUT: Réaliser un jeu de l'oie
+//ENTREES: La validation, et la valeure du dé
+//SORTIES: Le jeu nous dit se qu'il se passe
+
+CONST // Les constantes désignent les "cases spéciale" du jeu
+	
+	debut <- 0
+	fin <- 66
+	double <- 9
+	sixtroi <- 63
+	Gameover <- 58
+	minD <- 2
+	maxD <- 12
+
+VAR
+	de: ENTIER
+	joueur: ENTIER
+// "de" = le jet du dé, "joueur" = le placement du joueur 
+
+DEBUT
+
+	de <- 0
+	joueur <- 0
+
+	ECRIRE '           LE JEU DE LOIE               '
+	ECRIRE '         Appuyer sur entrer !           '
+	LIRE
+
+	ECRIRE 'Votre Oie est a la case départ: ',joueur
+
+	REPETER //Grande boucle  elle se répete jusqu'a se que joueur = 66(fin).
+
+		ECRIRE 'Appuyer sur entrer pour continuer'
+		LIRE
+		ECRIRE 'lancez les deux dés'
+		LIRE de
+
+		REPETER //Petite boucle elle se répete si le joueurs rentre un nombre plus petit que 2 ou plus grand que 12
+
+
+			ECRIRE ('Vous etes sur la case ','[',joueur,']', 'Appuyer sur entrer pour continuer');
+			ECRIRE ('lancez les deux des (Choisiez un nombre entre 2 et 12)');
+			LIRE (de);
+			SI (de > maxD) ET (de < minD) AlORS
+				ECRIRE ' Entrez un nombre valide !';
+						
+		JUSQU'A ((de <= maxD) ET (de >= minD));
+
+
+		joueur <-(de + joueur)
+
+		ECRIRE 'Bravo ! vous allez jusqu'a la case ','[',joueur,']',' Voyons se qui se passe ici...'
+
+		SI( joueur mod double = 0 ) ET ( joueur <> sixtroi ) ALORS
+			joueur <- de + joueur
+			ECRIRE '[*2]'
+			ECRIRE 'Incoyable ! votre jet est doublé '
+		FINSI
+
+		SI (joueur = Gameover) ALORS
+			joueur <- 0
+			ECRIRE '[X]'
+			ECRIRE 'Oh non ! vous étes sur la case téte de mort ! '
+			ECRIRE 'Vous retournez a la case depart... '
+		FINSI
+
+		SI (joueur > fin) ALORS
+			joueur <- (joueur - (joueur - fin))
+			ECRIRE 'Oula vous allez trop loin !'
+			ECRIRE 'Retournez donc a la case: ',joueur
+		FINSI
+
+	JUSQU'A (joueur = fin)
+	FINREPETER
+
+	ECRIRE 'Félicitation ! Vous étes a la case [66] vous avez donc gagné ! Bravo !'
+
+FIN
+}
+
 program Jeu_De_Loie;
 
 uses crt;
@@ -115,24 +115,19 @@ BEGIN
 	REPEAT//Grande boucle repeat, elle se répete jusqu'a se que joueur = 66(fin).
 
 		TextColor ( white );
-		begin
+		begin	
 
-			REPEAT//Petite boucle repeat elle se répete si le joueurs rentre un nombre plus petit que 2 ou plus grand que 12
 
-				begin	
+			writeln ('Vous etes sur la case ',' [',joueur,'] ', 'Appuyer sur entrer pour continuer');
+			readln ();
+			writeln ('lancez les deux des');
+			readln ();
+			de:=random(10)+2;
+			writeln ('Vos des = ',de);
 
-					writeln ('Vous etes sur la case ',' [',joueur,'] ', 'Appuyer sur entrer pour continuer');
-					readln ();
-					writeln ('lancez les deux des (Choisiez un nombre entre 2 et 12)');
-					readln (de);
-					if ((de > maxD) and (de < minD)) then
-						writeln (' Entrez un nombre valide !');
-						
-				end
-				until ((de <= maxD) and (de >= minD));
 
-				joueur := joueur + de;// ont ajoute le résultat des dés au placement du joueur (éxemple: si de = 4 et joueur = 2 alors joueur = 6)
-					writeln ('Vous allez jusqu a la case ',' [ ' ,joueur, ' ] ',' Voyons se qui se passe ici...');
+			joueur := joueur + de;// ont ajoute le résultat des dés au placement du joueur (éxemple: si de = 4 et joueur = 2 alors joueur = 6)
+			writeln ('Vous allez jusqu a la case ',' [ ' ,joueur, ' ] ',' Voyons se qui se passe ici...');
 
 			if( joueur mod double = 0 ) AND ( joueur <> sixtroi ) AND (joueur < fin) then{Si joueur est un multiple de 9 qu'il n'es pas 63 
 			et que ce n'es pas un nombre plus grand que 66 Le jet deviens donc double}

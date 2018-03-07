b725dac0cd71bd75bfbce618c8e76baa776cb11d
GaetanGalati <32631949+GaetanGalati@users.noreply.github.com>
2018-03-07 11:04:35 +0100

    Update Morpion.pas

--- Morpion/Morpion.pas
+++ Morpion/Morpion.pas
@@ -1,16 +1,16 @@
 PROGRAM  morpion;
+uses crt;
 
  //BUT:Jouer au morpion	
  //ENTREE:La case choisi
  //SORTIE:Gagne ou non et indique quel joueur gagne une manche
 
 TYPE
-	grid = array [1..3,1..3] of INTEGER;
+	grid = array [1..3,1..3] of char;
 
 
 CONST
 	NB_MANCHE : INTEGER = 3;
-	trais : CHAR = '-';
 VAR
 	Start : INTEGER;
 	grille : grid;
@@ -27,6 +27,7 @@ BEGIN
      for x:=1 to 3 do
      	for y:=1 to 3 do
      		grille[x,y]:= '-';
+     		writeln (grille[x,y])
 END;
 
 procedure party(var player1,player2:STRING);
@@ -43,8 +44,8 @@ BEGIN
 				readln (x);
 				writeln(player1,'choisiez une colonne (de 1 a 3)');
 				readln (y);
-				grille[x,y]<-'O';
-				start <- 2;
+				grille[x,y] :='O';
+				start := 2;
 			END;
 	If start = 2 then
 	While grille[x,y] <> '-' do
@@ -53,51 +54,95 @@ BEGIN
 			readln (x);
 			writeln(player1,'choisiez une colonne (de 1 a 3)');
 			readln (y);
-			grille[x,y]<-'X';
-			start <- 1;
+			grille[x,y]:='X';
+			start := 1;
 		END;
 
 END;
 
-function win (var grid, player1,player2: string)
+procedure win (var grille : grid);
 VAR 
-	Win_player1 : INTEGER
-	Win_player2 : INTEGER
+	Win_player1 : INTEGER;
+	Win_player2 : INTEGER;
+	player1 : string;
+	player2 : string;
 
 BEGIN
-	IF grid[1,1]='O' and grid[2,1]='O' and grid[3,1]='O'; 
-	OR grid[1,2]='O' and grid[2,2]='O' and grid[3,2]='O';
-	OR grid[1,3]='O' and grid[2,3]='O' and grid[3,3]='O';
-	OR grid[1,1]='O' and grid[2,2]='O' and grid[3,3]='O';
-	OR grid[3,1]='O' and grid[2,2]='O' and grid[1,3]='O'; 
+
+	//case grille of
+	//begin
+   		if (grille[1,1]='O') and (grille[2,1]='O') and (grille[3,1]='O') then
+   			begin 	
+   				writeln(player1,' Remporte la manche ! bravo a lui !');
+   				Win_Player1 := Win_Player1 +1
+   			end
+   		else if (grille[1,2]='O') and (grille[2,2]='O') and (grille[3,2]='O') then
+   			begin 	
+   				writeln(player1,' Remporte la manche ! bravo a lui !');
+   				Win_Player1 := Win_Player1 +1
+   			end
+   		else if (grille[1,3]='O') and (grille[2,3]='O') and (grille[3,3]='O') then
+   			begin 	
+   				writeln(player1,' Remporte la manche ! bravo a lui !');
+   				Win_Player1 := Win_Player1 +1
+   			end
+   		else if (grille[1,1]='O') and (grille[2,2]='O') and (grille[3,3]='O') then
+   			begin 	
+   				writeln(player1,' Remporte la manche ! bravo a lui !');
+   				Win_Player1 := Win_Player1 +1
+   			end
+   		else if (grille[3,1]='O') and (grille[2,2]='O') and (grille[1,3]='O') then
+   			begin 	
+   				writeln(player1,' Remporte la manche ! bravo a lui !');
+   				Win_Player1 := Win_Player1 +1;
+   			end;
+
+
+
+   	 	if (grille[1,1]='X') and (grille[2,1]='X') and (grille[3,1]='X') then
+   			begin 	
+   				writeln(player2,' Remporte la manche ! bravo a lui !');
+   				Win_Player2 := Win_Player2 +1
+   			end
+   		else if (grille[1,2]='X') and (grille[2,2]='X') and (grille[3,2]='X')then
+   			begin 	
+   				writeln(player2,' Remporte la manche ! bravo a lui !');
+   				Win_Player2 := Win_Player2 +1
+   			end
+   		else if (grille[1,3]='X') and (grille[2,3]='X') and (grille[3,3]='X') then
+   			begin 	
+   				writeln(player2,' Remporte la manche ! bravo a lui !');
+   				Win_Player2 := Win_Player2 +1
+   			end
+   		else if (grille[1,1]='X') and (grille[2,2]='X') and (grille[3,3]='X') then
+   			begin 	
+   				writeln(player2,' Remporte la manche ! bravo a lui !');
+   				Win_Player2 := Win_Player2 +1
+   			end
+   		else if (grille[3,1]='X') and (grille[2,2]='X') and (grille[1,3]='X') then
+   			begin 	
+   				writeln(player2,' Remporte la manche ! bravo a lui !');
+   				Win_Player2 := Win_Player2 +1;
+   			end;
+ 
+
 	
-	then
-		ECRIRE(player1&" Remporte la manche ! bravo a lui !");
-		Win_Player1 := Win_Player1 +1;
-	end;
 
-	IF grid[1,1]='X' and grid[2,1]='X' and grid[3,1]='X'; 
-	OR grid[1,2]='X' and grid[2,2]='X' and grid[3,2]='X';
-	OR grid[1,3]='X' and grid[2,3]='X' and grid[3,3]='X';
-	OR grid[1,1]='X' and grid[2,2]='X' and grid[3,3]='X';
-	OR grid[3,1]='X' and grid[2,2]='X' and grid[1,3]='X'; 
-	
-	then
-		writeln(player2&'Remporte la manche ! bravo a lui !');
-		Win_Player2 := Win_Player2 +1;
-	end;
+	IF (Win_Player1 = NB_MANCHE) THEN
+		BEGIN
+			writeln ('Incroyable ',player1,'A gagngez une partie de morpion ! Il est trop fort !')
+		end
 
-	IF Win_Player1 = NB_MANCHE THEN
-		writeln ('Incroyable '&player1&'A gagngez une partie de morpion ! Il est trop fort !');
+		ELSE IF (Win_Player2 = NB_MANCHE) THEN
+		begin
+			writeln ('Incroyable ',player2,'A gagngez une partie de morpion ! Il est trop fort !')
+		end
 
-		ELSE IF Win_Player2 = NB_MANCHE THEN
-			writeln ('Incroyable '&player2&'A gagngez une partie de morpion ! Il est trop fort !');
-
-				else IF Win_Player2 = Win_Player1
-				writeln ('Egalité ! ');
-			END;
-		END;
-	END;
+		else IF (Win_Player2 = Win_Player1) then
+		begin
+			writeln ('Egalité ! ');
+		end;
+END;
 
 	
 
@@ -105,7 +150,7 @@ BEGIN
  VAR
 
  	Player1,Player2 : string;
- 	tabgrid : array [1..3,1..3] of INTEGER;
+ 	tabgrid : array [1..3,1..3] of char;
 
 
 BEGIN
@@ -113,25 +158,28 @@ BEGIN
 	randomize;
 	Start :=random(2);
 
-	writeln ("Joueur 1 rentrez votre nom SVP");
+	writeln ('Joueur 1 rentrez votre nom SVP');
 	readln (player1);
-	writeln ("Joueur 2 rentrez votre nom SVP");
+	writeln ('Joueur 2 rentrez votre nom SVP');
 	readln (player2);
 
-	writeln (Player1&" VS "&player2);
+	writeln (Player1,' VS ',player2);
 		clrscr;
 
 		if start = 1 then
-				writeln("le joueur qui commence : "&player1);
-				writeln("Vous controlez les O");
+			begin
+				writeln('le joueur qui commence : ',player1);
+				writeln('Vous controlez les O')
+			end
 
 			else if start = 2 then
-				writeln("le joueur qui commence  : "&player2);
-				writeln("Vous controlez les X");
+			begin
+				writeln('le joueur qui commence  : ',player2);
+				writeln('Vous controlez les X');
 			end;
-		end;
 	
 	The_Grid(tabGrid);
-	party(Player1,Player2,tabgrid,start);
-	win(tabGrid,Player1,Player2);
+	party(Player1,Player2);
+	win(grille);
+	readln()
 END.
